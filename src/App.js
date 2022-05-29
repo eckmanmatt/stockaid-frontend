76a6f01dfb432157a11fef50a2e3dc8ef2c78a77
Matt Eckman <matteckman@Matts-MacBook-Air.local>
2022-05-29 09:24:53 -0400

    added simple states and organized known divs

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+
+  const [stock, setStock] = useState('')
+  const [symbol, setSymbol] = useState()
+  const [shortName,setShortName] = useState()
+  const [marketPrice, setMarketPrice] = useState()
+  const [marketChange, setMarketChange] = useState()
+
+
   return (
     <>
       <div className = 'header'>
-        <h1>hello world</h1>
+        <h1>Stock[AID]</h1>
+        <h2>The Future of Stock Marketing</h2>
+      </div>
+
+      <div className = 'addNew'>
+        <button>Add Ticker</button>
       </div>
 
       <div className = 'portfolio'>
