042a66b1682994de74db053603cfb2ba1aabd2e6
Matt Eckman <matteckman@matts-air.lan>
2022-05-31 15:44:31 -0400

    working on adding stock function to update states

--- src/App.js
+++ src/App.js
@@ -7,10 +7,23 @@ const App = () => {
 
   const [stock, setStock] = useState('')
   const [stocks, setStocks] = useState([])
-  const [newSymbol, setNewSymbol] = useState()
-  const [newShortName, setNewShortName] = useState()
-  const [newMarketPrice, setNewMarketPrice] = useState()
-  const [newMarketChange, setNewMarketChange] = useState()
+  const [newSymbol, setNewSymbol] = useState('')
+  const [newShortName, setNewShortName] = useState('')
+  const [newMarketPrice, setNewMarketPrice] = useState('')
+  const [newMarketChange, setNewMarketChange] = useState('')
+
+
+  //API connection
+  const options = {
+  method: 'GET',
+  url: 'https://yh-finance.p.rapidapi.com/stock/v2/get-summary',
+  params: {symbol: newSymbol},
+  headers: {
+    'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
+    'X-RapidAPI-Key': '314777666cmsha0de50e0f014d15p144d54jsn22d0daba111e'
+  }
+};
+
 
 
 
@@ -30,9 +43,20 @@ const handleDeleteStock = (stockData) => {
     })
 }
 
+const handleInputStock = (event) => {
+  setNewSymbol(event.target.value)
+}
 
 const handleNewStock = (event) => {
   event.preventDefault()
+  axios.request(options).then(function (response) {
+    // setNewSymbol(response.data.price.symbol)
+    setNewShortName(response.data.price.shortName)
+    setNewMarketPrice(response.data.price.regularMarketPrice.raw)
+    setNewMarketChange(response.data.price.regularMarketChange.raw)
+  }).catch(function (error) {
+    console.error(error);
+  }).then((response) => {
   axios.post(
       'https://stockaid-back-end.herokuapp.com/stocks',
       {
@@ -48,6 +72,7 @@ const handleNewStock = (event) => {
             setStocks(response.data)
         })
     })
+})
 }
 
 const handleUpdate = (event, stockData) => {
@@ -79,23 +104,24 @@ const handleUpdate = (event, stockData) => {
     </div>
 
     <div className = 'addNew'>
-      <input id='input' type="text" placeholder='Search for ticker...'/>
-      <button id='button'>Add Ticker to Portfolio</button>
+      <form onSubmit={handleNewStock}>
+        <input id='input' type="text" placeholder='Search for ticker.' onChange={handleInputStock}/>
+        <input type='submit' value='Add Stock'/>
+      </form>
     </div>
 
     <div className = 'portfolio'>
       <h2>My Portfolio</h2>
-      <section className = 'card'>
+      <section>
         <ul>
           {stocks.map((stock) => {
-            return <li>
+            return <li className = 'card'>
                     <h3>{stock.symbol}</h3>
                     <h2>{stock.shortName}</h2>
-                    <h4>{stock.marketPrice}</h4>
-                    <h4>{stock.marketChange}</h4>
+                    <h4>Market Price:{stock.marketPrice}</h4>
+                    <h4>Market Change: {stock.marketChange}</h4>
                     <button onClick = {(event) => {handleUpdate(stock)}}>Update</button>
                     <button onClick = {(event) => {handleDeleteStock(stock)}}>Remove</button>
-
                   </li>
 
                 })}
