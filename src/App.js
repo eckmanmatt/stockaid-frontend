81361692efa77cee9e5728c7b9835a0fca9387cb
Matthew Downey <matthewdowney@Matthews-MacBook-Air.local>
2022-05-31 19:21:41 -0500

    added confirmation for and toggle function

--- src/App.js
+++ src/App.js
@@ -1,103 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const App = () => {
 
-  const [stock, setStock] = useState('')
   const [stocks, setStocks] = useState([])
   const [newSymbol, setNewSymbol] = useState()
   const [newShortName, setNewShortName] = useState()
   const [newMarketPrice, setNewMarketPrice] = useState()
   const [newMarketChange, setNewMarketChange] = useState()
   const [recommendations, setRecommendations] = useState()
+  const [toggleConfirmationForm, setToggleConfirmationForm] = useState(false)
 
-
-  //API connection
-  const options = {
-  method: 'GET',
-  url: 'https://yh-finance.p.rapidapi.com/stock/v2/get-summary',
-  headers: {
-    'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
-    'X-RapidAPI-Key': '314777666cmsha0de50e0f014d15p144d54jsn22d0daba111e'
-  }
-};
-
-
-useEffect(() => {
-  axios.get('https://stockaid-back-end.herokuapp.com/stocks').then((response) => {
-    setStocks(response.data)
-  })
-}, [])
-
-
-const handleDeleteStock = (stockData) => {
-  axios.delete(`https://stockaid-back-end.herokuapp.com/stocks/${stockData._id}`)
-    .then(() => {
-      axios.get('https://stockaid-back-end.herokuapp.com/stocks')
-            .then((response) => {
-              setStocks(response.data)
-            })
+  useEffect(() => {
+    axios.get('https://stockaid-back-end.herokuapp.com/stocks').then((response) => {
+      setStocks(response.data)
     })
-}
+  }, [])
 
-const handleInputStock = (event) => {
-  setNewSymbol(event.target.value)
-}
 
-const handleNewStock = (event) => {
-  event.preventDefault()
-  axios.request(`https://api.polygon.io/v3/reference/tickers?ticker=${newSymbol}&active=true&sort=ticker&order=asc&limit=10&apiKey=93iCvd9wrOUYmz69ZzVj6w32X5rPT2bR`).then((response) => {
-    setNewShortName(response.data.results[0].name)
-  }).then((response) => {
-    axios.request(`https://api.polygon.io/v2/aggs/ticker/${newSymbol}/prev?adjusted=true&apiKey=93iCvd9wrOUYmz69ZzVj6w32X5rPT2bR`).then(function (response) {
-    setNewMarketPrice(response.data.results[0].c)
-    setNewMarketChange((response.data.results[0].c - response.data.results[0].o) / response.data.results[0].o)
-  }).catch(function (error) {
-    console.error(error);
-  })
-  })
-}
-
-const confirmStock = (event) => {
-  event.preventDefault()
-  axios.post(
-    'https://stockaid-back-end.herokuapp.com/stocks',
-    {
-      symbol: newSymbol,
-      shortName: newShortName,
-      marketPrice: newMarketPrice,
-      marketChange: newMarketChange
-    }
-  ).then((response) => {
-    axios
-      .get('https://stockaid-back-end.herokuapp.com/stocks')
-      .then((response) => {
-          setStocks(response.data)
+  const handleDeleteStock = (stockData) => {
+    axios.delete(`https://stockaid-back-end.herokuapp.com/stocks/${stockData._id}`)
+      .then(() => {
+        axios.get('https://stockaid-back-end.herokuapp.com/stocks')
+              .then((response) => {
+                setStocks(response.data)
+              })
       })
-  })
-}
+  }
 
-const handleUpdate = (event, stockData) => {
-  event.preventDefault()
-  axios
-    .put(`https://stockaid-back-end.herokuapp.com/stocks/${stockData._id}`,
+  const handleInputStock = (event) => {
+    setNewSymbol(event.target.value)
+  }
+
+  const handleNewStock = (event) => {
+    event.preventDefault()
+    axios.request(`https://api.polygon.io/v3/reference/tickers?ticker=${newSymbol}&active=true&sort=ticker&order=asc&limit=10&apiKey=93iCvd9wrOUYmz69ZzVj6w32X5rPT2bR`).then((response) => {
+      setNewShortName(response.data.results[0].name)
+    }).then((response) => {
+      axios.request(`https://api.polygon.io/v2/aggs/ticker/${newSymbol}/prev?adjusted=true&apiKey=93iCvd9wrOUYmz69ZzVj6w32X5rPT2bR`).then(function (response) {
+      setNewMarketPrice(response.data.results[0].c)
+      setNewMarketChange((response.data.results[0].c - response.data.results[0].o) / response.data.results[0].o)
+    }).catch(function (error) {
+      console.error(error);
+    })
+    })
+    handleToggleConfirmationForm()
+  }
+
+  const confirmStock = (event) => {
+    event.preventDefault()
+    axios.post(
+      'https://stockaid-back-end.herokuapp.com/stocks',
       {
         symbol: newSymbol,
         shortName: newShortName,
         marketPrice: newMarketPrice,
         marketChange: newMarketChange
       }
-    ).then(() => {
-        axios
-          .get('https://stockaid-back-end.herokuapp.com/stocks')
-          .then((response) => {
-            console.log(response.data);
-              setStocks(response.data)
-          })
+    ).then((response) => {
+      axios
+        .get('https://stockaid-back-end.herokuapp.com/stocks')
+        .then((response) => {
+            setStocks(response.data)
+        })
     })
-}
+    handleToggleConfirmationForm()
+  }
+
+  const handleUpdate = (event, stockData) => {
+    event.preventDefault()
+    axios
+      .put(`https://stockaid-back-end.herokuapp.com/stocks/${stockData._id}`,
+        {
+          symbol: newSymbol,
+          shortName: newShortName,
+          marketPrice: newMarketPrice,
+          marketChange: newMarketChange
+        }
+      ).then(() => {
+          axios
+            .get('https://stockaid-back-end.herokuapp.com/stocks')
+            .then((response) => {
+              console.log(response.data);
+                setStocks(response.data)
+            })
+      })
+  }
 
   // const currentRecommendations = {
   //   method: 'GET',
@@ -120,6 +109,14 @@ const handleUpdate = (event, stockData) => {
   //   }, 1000)
   // }, [])
 
+  const handleToggleConfirmationForm = () => {
+    if (toggleConfirmationForm) {
+      setToggleConfirmationForm(false)
+    } else {
+      setToggleConfirmationForm(true)
+    }
+  }
+
   return (
     <>
     <div className = 'header'>
@@ -132,7 +129,16 @@ const handleUpdate = (event, stockData) => {
         <input id='input' type="text" placeholder='Search for ticker.' onChange={handleInputStock}/>
         <input type='submit' value='Add Stock'/>
       </form>
-      <button onClick={confirmStock}>CONFIRM STOCK</button>
+      {toggleConfirmationForm ?
+        <div className='confirmation-form'>
+          <br />
+          <h3>{newSymbol}</h3>
+          <h3>{newShortName}</h3>
+          <h3>{newMarketPrice}</h3>
+          <h3>{newMarketChange}</h3>
+          <button onClick={confirmStock}>CONFIRM STOCK</button>
+        </div>
+      : null}
     </div>
 
     <div className = 'portfolio'>
@@ -140,18 +146,19 @@ const handleUpdate = (event, stockData) => {
       <section>
         <ul>
           {stocks.map((stock) => {
-            return <li className = 'card'>
-                    <h3>{stock.symbol}</h3>
-                    <h2>{stock.shortName}</h2>
-                    <h4>Market Price:{stock.marketPrice}</h4>
-                    <h4>Market Change: {stock.marketChange}</h4>
-                    <div id = 'cardButton'>
-                      <button onClick = {(event) => {handleUpdate(stock)}}>Update</button>
-                      <button onClick = {(event) => {handleDeleteStock(stock)}}>Remove</button>
-                    </div>
-                  </li>
-
-                })}
+            return (
+              <li className='card' key={stock._id}>
+                <h3>{stock.symbol}</h3>
+                <h2>{stock.shortName}</h2>
+                <h4>Market Price: {stock.marketPrice}</h4>
+                <h4>Market Change: {stock.marketChange}</h4>
+                <div id = 'cardButton'>
+                  <button onClick = {(event) => {handleUpdate(stock)}}>Update</button>
+                  <button onClick = {(event) => {handleDeleteStock(stock)}}>Remove</button>
+                </div>
+              </li>
+            )
+          })}
         </ul>
       </section>
     </div>
