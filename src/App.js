7f303481b47d2f537a429b636ddf9abf9c271e45
Matt Eckman <matteckman@matts-air.lan>
2022-06-01 12:37:49 -0400

    working on update function

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ const App = () => {
   const [newMarketChange, setNewMarketChange] = useState()
   const [recommendations, setRecommendations] = useState()
   const [toggleConfirmationForm, setToggleConfirmationForm] = useState(false)
+  const [toggleUpdateForm, setToggleUpdateForm] = useState(false)
+
+  const [updatedStock, setUpdatedStock] = useState()
 
   useEffect(() => {
     axios.get('https://stockaid-back-end.herokuapp.com/stocks').then((response) => {
@@ -31,6 +34,10 @@ const App = () => {
 
   const handleInputStock = (event) => {
     setNewSymbol(event.target.value)
+  }
+
+  const handleUpdatedStock = (event) => {
+    setUpdatedStock(event.target.value)
   }
 
   const handleNewStock = (event) => {
@@ -68,25 +75,27 @@ const App = () => {
     handleToggleConfirmationForm()
   }
 
-  const handleUpdate = (event, stockData) => {
-    event.preventDefault()
-    axios
-      .put(`https://stockaid-back-end.herokuapp.com/stocks/${stockData._id}`,
+  const handleUpdate = (event) =>{
+    // event.preventDefault()
+    axios.request(`https://api.polygon.io/v3/reference/tickers?ticker=${updatedStock}&active=true&sort=ticker&order=asc&limit=10&apiKey=93iCvd9wrOUYmz69ZzVj6w32X5rPT2bR`).then((response) => {
+      setNewShortName(response.data.results[0].name)
+      setNewMarketPrice(response.data.results[0].c)
+      setNewMarketChange((response.data.results[0].c - response.data.results[0].o) / response.data.results[0].o).then(() => {
+      axios.put(`https://stockaid-back-end.herokuapp.com/stocks/${stocks._id}`,
         {
-          symbol: newSymbol,
+          symbol: updatedStock,
           shortName: newShortName,
           marketPrice: newMarketPrice,
           marketChange: newMarketChange
-        }
-      ).then(() => {
-          axios
-            .get('https://stockaid-back-end.herokuapp.com/stocks')
-            .then((response) => {
-              console.log(response.data);
-                setStocks(response.data)
+        })
+        .then(() => {
+            axios.get(`https://stockaid-back-end.herokuapp.com/stocks`).then((response) => {
+              setStocks(response.data)
             })
-      })
-  }
+        })
+    handleToggleUpdateForm()
+    })
+  })}
 
   // const currentRecommendations = {
   //   method: 'GET',
@@ -116,6 +125,14 @@ const App = () => {
       setToggleConfirmationForm(true)
     }
   }
+  const handleToggleUpdateForm = () => {
+    if (toggleUpdateForm) {
+      setToggleUpdateForm(false)
+    } else {
+      setToggleUpdateForm(true)
+    }
+  }
+
 
   return (
     <>
@@ -153,8 +170,15 @@ const App = () => {
                 <h4>Market Price: {stock.marketPrice}</h4>
                 <h4>Market Change: {stock.marketChange}</h4>
                 <div id = 'cardButton'>
-                  <button onClick = {(event) => {handleUpdate(stock)}}>Update</button>
-                  <button onClick = {(event) => {handleDeleteStock(stock)}}>Remove</button>
+                {toggleUpdateForm ?
+                  <>
+                  New Ticker: <input type="text"  onKeyUp={handleUpdatedStock}/><br/>
+                  <button onClick={(event) => {handleUpdate(stock)}}>Confirm Update</button><br/></>  :
+                  <>
+                  </>
+                }
+                <button onClick = {(event) => {handleToggleUpdateForm(stock)}}>Update</button>
+                <button onClick = {(event) => {handleDeleteStock(stock)}}>Remove</button>
                 </div>
               </li>
             )
